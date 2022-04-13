9134accec3b360b16b9d6b1be2ae73bc6284b0d2
unknown <emranulfarhad@gmail.com>
2022-04-13 15:34:33 +0600

    serivecs done

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,29 +1,29 @@
 import React from 'react';
+import './Serivces.css'
 
 const Services = (props) => {
-    const {name, price , id, img , description} = props.service
+    const { name, price, id, img, description } = props.service
 
     return (
-        <div >
-            
-               <div className="container">
-                            <div className="wsk-cp-product">
-                                <div className="wsk-cp-img">
-                                    <img src={img} alt="Product" className="img-responsive" />
-                                </div>
-                                <div className="wsk-cp-text">
-                                    <div className="category">
-                                        <span> {price} </span>
-                                    </div>
-                                    <div className="title-product">
-                                        <h3>Tire car wheel </h3>
-                                    </div>
-                                    <div className="description-prod">
-                                        <p> {description} </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div>
+            <div className="container servicesD">
+                <div class="collection">
+                    <img src={img} alt="" />
+                    <h3 className='serviceTExt'>{name}</h3>
+                    <p className='servicePrice'> ${price} </p>
+                    <div >
+                        <p class="collecting-details">
+                            {description}
+                        </p>
+                    </div>
+                    {/* <div className='servieBUTTON'>
+                    <a class="blog-btn" href="#"> Book Now  </a>
+                    </div> */}
+                </div>
+                <div className='servieBUTTON'>
+                    <a class="blog-btn" href="#"> Book Now  </a>
+                    </div>
+            </div>
         </div>
     );
 };
